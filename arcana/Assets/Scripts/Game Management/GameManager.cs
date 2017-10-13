ebcefeb4501d7443d27fd4bc0f363c14b3c2fdfc
Christopher Katcher (RIT Student) <cak1352@ad.rit.edu>
2017-10-13 17:00:23 -0400

    Removed infinite jumping

--- arcana/Assets/Scripts/Game Management/GameManager.cs
+++ arcana/Assets/Scripts/Game Management/GameManager.cs
@@ -28,8 +28,8 @@ namespace Arcana
         private bool grounded = true;
         private Rigidbody2D wizzard1_rb;
 
-        private float jumpForce = 300f;
-        private float moveForce = 365f;
+        private float jumpForce = 500f;
+        private float moveForce = 50f;
 
         public bool fly_mode = false;
 
@@ -52,7 +52,7 @@ namespace Arcana
                 wizzard1_rb.velocity = new Vector2(Mathf.Sign(wizzard1_rb.velocity.x) * max_speed, wizzard1_rb.velocity.y);
             }
             
-            if(!grounded)
+            if(!grounded && wizzard1_rb.velocity.y == 0.0f)
             {
                 wizzard1_rb.AddForce(new Vector2(0f, jumpForce));
                 grounded = true;
