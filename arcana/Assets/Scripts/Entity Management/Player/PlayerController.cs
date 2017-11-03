fe73b0c6aed183abcb2645bd29646d097e1c0e40
Ian Effendi <effendiian@gmail.com>
2017-11-02 20:44:55 -0400

    Edit UI

--- arcana/Assets/Scripts/Entity Management/Player/PlayerController.cs
+++ arcana/Assets/Scripts/Entity Management/Player/PlayerController.cs
@@ -189,7 +189,6 @@ namespace Arcana.Entities.Attributes
                     "Images/wizardBodyA",
                     "Materials/WizzardBodyAMat");
                 renderer.InitializeRenderer();
-
             }
             
             return renderer;
